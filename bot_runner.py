28891520ea3b43fdfa8815be61240007488e534c
Benji Pelletier <pelletierbenji@gmail.com>
2017-08-24 23:56:59 -0400

    Genius API addition

--- bot_runner.py
+++ bot_runner.py
@@ -1,17 +1,24 @@
+import urllib
+import requests
+import json
 import praw
 import config
 
 def bot_login():
 	return praw.Reddit(username = config.username,
-				passsword = config.password,
-				client_id = config.client_id,
-				client_secret = config.client_secret,
-				user_agent = "LyricTalkBot v 1.0")
+					   passsword = config.password,
+					   client_id = config.client_id,
+					   client_secret = config.client_secret,
+					   user_agent = "LyricTalkBot v 1.0")
 
 def run_bot(r):
-	for comment in r.subreddit('LyricTalk').comments(limits=25):
-		if "test" in comment.body:
-		print("donion rings")
+	for comment in r.subreddit('LyricTalk').stream.comments():
+		print(comment.body)
+		query = urllib.urlencode({'q': comment.body})
+		r = requests.get('https://api.genius.com/search?' + query, headers={'Authorization': 'Bearer ' + config.access_token})
+		comment.reply("KEK")
+		print(r.json())
+
 
 login_instance = bot_login()
 run_bot(login_instance)
