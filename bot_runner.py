aba5882d85c1bc1f0a34d3c97ffb386ed8ac877d
Benji Pelletier <pelletierbenji@gmail.com>
2017-08-30 22:42:24 -0400

    Testing multiple reply issue

--- bot_runner.py
+++ bot_runner.py
@@ -4,20 +4,28 @@ import json
 import praw
 import config
 
+replyTemplate = ""
+
 def bot_login():
 	return praw.Reddit(username = config.username,
-					   passsword = config.password,
+					   password = config.password,
 					   client_id = config.client_id,
 					   client_secret = config.client_secret,
 					   user_agent = "LyricTalkBot v 1.0")
 
 def run_bot(r):
 	for comment in r.subreddit('LyricTalk').stream.comments():
-		print(comment.body)
-		query = urllib.urlencode({'q': comment.body})
-		r = requests.get('https://api.genius.com/search?' + query, headers={'Authorization': 'Bearer ' + config.access_token})
-		comment.reply("KEK")
-		print(r.json())
+		if comment.is_root:
+			query = urllib.urlencode({'q': comment.body})
+			req = requests.get('https://api.genius.com/search?' + query, headers={'Authorization': 'Bearer ' + config.access_token})
+			resp = req.json()
+			comment.refresh()
+			print(comment.replies.list())
+			if len(resp['response']['hits']) > 0 and len(comment.replies.list()) == 0:
+				comment.reply('"' + comment.body + '"' + ' from [' + resp['response']['hits'][0]['result']['title'] + '](' + 
+								resp['response']['hits'][0]['result']['url'] + ')' + ' by [' + 
+								resp['response']['hits'][0]['result']['primary_artist']['name'] + '](' + 
+								resp['response']['hits'][0]['result']['primary_artist']['image_url'] + ')')
 
 
 login_instance = bot_login()
